8df2502c4a1d79cd3ff71d5e95147ee8e7430501
Dave Berman <dberman@ngpvan.com>
2015-05-24 22:09:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var request = require ('request') //find this somewhere - download? https://github.com/request/request
+request.post('https://www.fantrax.com/login.go?loginMsgId=b').form({j_username: 'dbb817@gmail.com', j_password: 'inserthere'}) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the fantrax league homepage.
+  }
+})
+
 var Fantrax = require('./api');
 var myFantrax = new Fantrax();
 
